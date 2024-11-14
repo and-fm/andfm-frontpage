2282090340336001dc214ab4d75e1a6c7d71db9e
npgy <me@np.gy>
2024-11-14 15:25:26 -0500

    some refactoring and changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, sharpImageService } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 // import react from "@astrojs/react";
 import compress from "astro-compress";
 
--- src/components/BodyImageRenderer.ts
+++ src/components/BodyImageRenderer.ts
@@ -1,41 +1,40 @@
-import { getImage } from "astro:assets"
+import { getImage } from "astro:assets";
 
-import type { Media } from "../../types/payload-types"
+import type { Media } from "../../types/payload-types";
 
 enum MediaType {
-    thumbnail,
-    sixteenByNineMedium,
-    sixteenByNineLarge,
+  thumbnail,
+  sixteenByNineMedium,
+  sixteenByNineLarge,
 }
 
-type MediaTypes = keyof typeof MediaType // "thumbnail" | "sixteenByNineMedium" | "sixteenByNineLarge"
+type MediaTypes = keyof typeof MediaType; // "thumbnail" | "sixteenByNineMedium" | "sixteenByNineLarge"
 
 interface Props {
-    media: Media, 
-    scaleWidth?: number, 
-    scaleHeight?: number, 
-    mediaType: MediaTypes,
-    alt: string,
+  media: Media;
+  scaleWidth?: number;
+  scaleHeight?: number;
+  mediaType: MediaTypes;
+  alt: string;
 }
 
-export default async ({media, mediaType, alt}: Props) => {
+export default async ({ media, mediaType, alt }: Props) => {
+  const sizes: Media["sizes"] = media?.sizes;
 
-    const sizes: Media["sizes"] = media?.sizes
+  if (!sizes) return null;
 
-    if (!sizes) return null
+  const sizeVariant = sizes[mediaType] || sizes?.thumbnail;
 
-    const sizeVariant = sizes[mediaType] || sizes?.thumbnail
+  const optimised = await getImage({
+    src: sizeVariant?.url as string,
+    width: sizeVariant?.width,
+    height: sizeVariant?.height,
+    alt: alt,
+  });
 
-    const optimised = await getImage({
-        src: sizeVariant?.url as string,
-        width: sizeVariant?.width,
-        height: sizeVariant?.height,
-        alt: alt,
-    })
+  const { src, attributes } = optimised;
 
-    const { src, attributes} = optimised
+  const { width, height } = attributes;
 
-    const {width, height} = attributes
-
-    return `<img onload="this.style.opacity=1" class="easeload w-full rounded-xl" src="${src}" width="${width}" height="${height}" alt="${alt}" />`
-}
+  return `<img onload="this.style.opacity=1" class="w-full rounded-xl" src="${src}" width="${width}" height="${height}" alt="${alt}" />`;
+};
