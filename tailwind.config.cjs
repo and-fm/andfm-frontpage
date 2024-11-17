3cafca720f997046611c1d1c5664fc58cec6036c
npgy <me@np.gy>
2024-11-17 11:02:24 -0500

    some adjustments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-		    colors: {
-				'and-primary': '#e07d3b',
-				'and-black': '#232323',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "surface-700": "#3d3d3d",
+      },
+    },
+  },
+  plugins: [],
+};
